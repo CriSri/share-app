c10834d194544acacc063570acb1b05a13476f54
RedboneStone <Redbonestone123@gmail.com>
2019-06-01 21:46:20 +0800

    添加default.less默认色表  完成底部布局 todo:完成home页面

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./index.less"
 export default class Footer extends React.Component{
     render(){
         return (
-            <div>
-                This is Footer
+            <div className="footer">
+               版权所有: CriSri(推荐使用google Chrome)以保证体验
             </div>
         )
     }
--- src/components/nav-left/index.js
+++ src/components/nav-left/index.js
@@ -11,6 +11,7 @@ export default class Navleft extends React.Component{
             menuTreeNode
         })
     }
+    //通过递归来实现对数据的遍历生成  如果子菜单为嵌套结构 则继续遍历
     renderMenu = (data)=>{
         return data.map((item)=>{
             if(item.children){
